fb082824a145a7bbe16b6c294744fd53131133dd
Ata Mahmoud <ata.ata250@hotmail.com>
2018-04-09 18:27:09 +0200

    adding API Models

---
+++ PaperBoy/PaperBoy/Models/NewCategoryType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PaperBoy.Models
+{
+    public enum NewCategoryType
+    {
+        Business,
+        Entertainment,
+        Health,
+        Politics,
+        ScienceAndTechnology,
+        Sports,
+        World
+    }
+}
