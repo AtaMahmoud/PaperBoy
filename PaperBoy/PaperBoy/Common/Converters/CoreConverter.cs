a46a4d188afc6fac523238c5074575a3469681af
Ata Mahmoud <ata.ata250@hotmail.com>
2018-06-07 20:12:08 +0200

    Event To Command Behavior impelementation

--- PaperBoy/PaperBoy/Common/Converters/CoreConverter.cs
+++ PaperBoy/PaperBoy/Common/Converters/CoreConverter.cs
@@ -33,4 +33,18 @@ namespace PaperBoy.Common.Converters
             return null;
         }
     }
+
+    public sealed class SelectedItemConverter:IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var eventArgs = value as SelectedItemChangedEventArgs;
+            return eventArgs.SelectedItem;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
